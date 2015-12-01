8075d94484d41de9d27969892d1987dfef00cad3
Sevak Sargsyan <ssargsyan@macadamian.com>
2015-12-01 19:09:59 +0400

    iOS: Crashlytics and Fabric integration * Created a simple splitview app for testing * Integrated Crashlytics and Fabric * Tried Crashlytics' custom logging methods and force-crashing (works fine) * Tried Fabric's Answers for logging custom events (works fine) * Note: Fabric requires dSYM file, which requires valid Apple ID keys * Discontinued. Keeping for further usage.

---
+++ Project/Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by Intern on 11/30/15.
+//  Copyright © 2015 Macadamian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
